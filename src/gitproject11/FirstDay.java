40cd2013e042ee7ccd76b32fde339f322b0fa666
Canan Ozdemir <crazyfordata.123@gmail.com>
2021-03-04 00:18:32 +0000

    second commit

--- src/gitproject11/FirstDay.java
+++ src/gitproject11/FirstDay.java
@@ -3,7 +3,9 @@ package gitproject11;
 public class FirstDay {
 
 	public static void main(String[] args) {
-     System.out.println("first day: 16:30");
+     System.out.println("first day: 00.15");
+     System.out.println("second commit");
+     
 	}
 
 }
