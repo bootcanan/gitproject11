016f6dc80226d78169391e2840059834a460d2c6
bootcanan <79773390+bootcanan@users.noreply.github.com>
2021-03-05 00:51:00 +0000

    Update FirstDay.java

--- src/gitproject11/FirstDay.java
+++ src/gitproject11/FirstDay.java
@@ -5,6 +5,7 @@ public class FirstDay {
 	public static void main(String[] args) {
      System.out.println("first day: 00.15");
      System.out.println("second commit");
+     System.out.println("00.49 second day");
      
 	}
 
