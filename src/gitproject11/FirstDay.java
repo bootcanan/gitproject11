eced96cc6c534b888f44d67045df7ca8d691311b
Canan Ozdemir <crazyfordata.123@gmail.com>
2021-04-21 12:47:17 +0100

    commit yapildi

--- src/gitproject11/FirstDay.java
+++ src/gitproject11/FirstDay.java
@@ -28,6 +28,7 @@ public class FirstDay {
         System.out.println("4 day and 12:25");
         System.out.println("4 day and 12:30");
            System.out.println("4 day and 12:40");
+           System.out.println("I do not want conflict");
         
 
 
